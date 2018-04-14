d1189b34278ac40995a7fd5fc73b915dcb8380da
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-13 22:02:15 -0500

    Agregar relación entre story y user

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,6 @@
 class Story < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true,
                     length: { maximum: 60 }
   validates_presence_of :content
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+
+  has_many :stories, dependent: :destroy
 end
