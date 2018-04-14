d004b3f2f318dad2848441997c1275c181062ef5
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-13 19:29:24 -0500

    Agregar tests para modelo Story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ApplicationRecord
+  validates :title, presence: true,
+                    length: { maximum: 60 }
+  validates_presence_of :content
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @story = Story.new(title: 'Foo Bar', content: 'Foo Baz')
+  end
+
+  test 'valid story' do
+    assert @story.valid?
+  end
+
+  test 'invalid without title' do
+    @story.title = nil
+    assert_not @story.valid?
+  end
+
+  test 'invalid if title length is greater than 60 characters' do
+    @story.title = 'a' * 61
+    assert_not @story.valid?
+  end
+
+  test 'invalud without content' do
+    @story.content = nil
+    assert_not @story.valid?
+  end
 end
