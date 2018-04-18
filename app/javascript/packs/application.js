fdb9916a06925ac084aad95db0e3b9d93c6d9fca
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-18 00:37:48 -0500

    Instalar webpacker

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,8 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import '../src/application.scss'
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
