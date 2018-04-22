2f8cc7ede855d4f6aa1d0c24d631e2dd1f2b508a
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-21 19:52:06 -0500

    Migrar javascript a webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
 import '../src/application.scss'
 
+import $ from 'jquery'
+
+window.jQuery = $
+window.$ = $
+
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 const application = Application.start()
 const context = require.context('controllers', true, /.js$/)
 application.load(definitionsFromContext(context))
