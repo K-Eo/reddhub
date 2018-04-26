a219bc8b5b33d341f7bbef7afb3418421e67c4a2
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-26 02:02:37 -0500

    Agregar estados: draft y published a stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,17 @@ class Story < ApplicationRecord
   validates :title, presence: true,
                     length: { maximum: 60 }, on: :with_meta
   validates_presence_of :content, on: :with_content
+
+  state_machine :state, initial: :draft do
+    event :publish do
+      transition :draft => :published
+    end
+
+    event :unpublish do
+      transition :published => :draft
+    end
+
+    state :published
+    state :draft
+  end
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -24,4 +24,26 @@ class StoryTest < ActiveSupport::TestCase
     @story.content = nil
     assert_not @story.valid?(:with_content)
   end
+
+  class StoryState < StoryTest
+    test "draft as initial state" do
+      @story.save
+      assert_equal "draft", @story.state
+    end
+
+    test "publish sets state to published" do
+      @story.save
+      assert_changes -> { @story.state }, from: "draft", to: "published" do
+        @story.publish
+      end
+    end
+
+    test "unpublish sets state to draft" do
+      @story.save
+      @story.publish
+      assert_changes -> { @story.state }, from: "published", to: "draft" do
+        @story.unpublish
+      end
+    end
+  end
 end
