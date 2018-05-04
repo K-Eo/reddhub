fb38ef7b1b8e74599595da1a6a3cf688f13615b7
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-04 16:01:53 -0500

    Agregar subtitle a historias

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -3,63 +3,86 @@ require "test_helper"
 class StoryTest < ActiveSupport::TestCase
   def setup
     @user = users(:eo)
-    @story = Story.new(title: "Foo Bar", content: "Foo Baz", user: @user)
+    @story = Story.new(
+      title: "",
+      subtitle: "",
+      content: "",
+      user: @user
+    )
   end
 
   test "valid story" do
     assert @story.valid?
   end
 
-  test "invalid without title" do
-    @story.title = nil
-    assert_not @story.valid?(:with_meta)
-  end
-
-  test "invalid if title length is greater than 60 characters" do
-    @story.title = "a" * 61
-    assert_not @story.valid?(:with_meta)
-  end
-
-  test "invalid without content" do
-    @story.content = nil
-    assert_not @story.valid?(:with_content)
-  end
-
-  class StoryState < StoryTest
-    test "returns true if state is public" do
+  class OnUpdate < StoryTest
+    def setup
+      super
+      @story.title = "Title"
+      @story.subtitle = "Subtitle"
+      @story.content = "# Content"
       @story.save
+    end
+
+    test "invalid without title" do
+      @story.title = nil
+      assert_not @story.valid?
+    end
+
+    test "invalid if title length is greater than 60 characters" do
+      @story.title = "a" * 61
+      assert_not @story.valid?
+    end
+
+    test "invalid without subtitle" do
+      @story.subtitle = nil
+      assert_not @story.valid?
+    end
+
+    test "invalid if subtitle is greated than 120 characters" do
+      @story.subtitle = "a" * 121
+      assert_not @story.valid?
+    end
+
+    test "invalid without content" do
+      @story.content = nil
+      assert_not @story.valid?
+    end
+  end
+
+  class StoryState < OnUpdate
+    def setup
+      super
+    end
+
+    test "returns true if state is public" do
       @story.publish
       assert @story.public?
       assert_not @story.draft?
     end
 
     test "returns true if state is draft" do
-      @story.save
       assert @story.draft?
       assert_not @story.public?
     end
 
     test "draft as initial state" do
-      @story.save
       assert_equal "draft", @story.state
     end
 
     test "publish sets state to published" do
-      @story.save
       assert_changes -> { @story.state }, from: "draft", to: "public" do
         @story.publish
       end
     end
 
     test "sets published_at when changing to published state" do
-      @story.save
       assert_changes -> { @story.published_at }, "Expected published_at to not be nil" do
         @story.publish
       end
     end
 
     test "sets published_at to nil when changing to draft state" do
-      @story.save
       @story.publish
       assert_changes -> { @story.published_at }, "Expected published_at to be nil" do
         @story.unpublish
@@ -68,7 +91,6 @@ class StoryTest < ActiveSupport::TestCase
     end
 
     test "unpublish sets state to draft" do
-      @story.save
       @story.publish
       assert_changes -> { @story.state }, from: "public", to: "draft" do
         @story.unpublish
