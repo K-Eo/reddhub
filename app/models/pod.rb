08a98c66421908709212f8ccdb1491849826d5ff
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-08 18:34:09 -0500

    Agregar validaciones a modelo Pod

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -1,3 +1,6 @@
 class Pod < ApplicationRecord
   belongs_to :user
+
+  validates :content, presence: true,
+                      length: { maximum: 256 }
 end
--- test/models/pod_test.rb
+++ test/models/pod_test.rb
@@ -1,7 +1,27 @@
-require 'test_helper'
+require "test_helper"
 
 class PodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:eo)
+    @pod = Pod.new(user: @user, content: "Foo bar")
+  end
+
+  test "is valid" do
+    assert @pod.valid?
+  end
+
+  test "is invalid without content" do
+    @pod.content = nil
+    assert_not @pod.valid?
+  end
+
+  test "is invalid if content is too long" do
+    @pod.content = "a" * 257
+    assert_not @pod.valid?
+  end
+
+  test "is invalid if has no user" do
+    @pod.user = nil
+    assert_not @pod.valid?
+  end
 end
