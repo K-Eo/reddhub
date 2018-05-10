ffdf529d52234133598edb9b5fbbda0900a362e9
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-10 15:47:00 -0500

    Agregar seeds para usuarios y pods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bilbo = User.new(email: "bilbo@mail.com", password: "qwerasdf", password_confirmation: "qwerasdf", username: "Bilbo", name: "Bilbo Baggins")
+bilbo.skip_confirmation!
+bilbo.save!
+
+50.times do
+  bilbo.pods.create!(content: Faker::Hobbit.quote)
+end
+
+thorin = User.new(email: "thorin@mail.com", password: "qwerasdf", password_confirmation: "qwerasdf", username: "Thorin", name: "Thorin Oakenshield")
+thorin.skip_confirmation!
+thorin.save!
+
+50.times do
+  thorin.pods.create!(content: Faker::Hobbit.quote)
+end
+
+puts "Seed OK"
