1e038ed354b57d57dc4ef7ac230d0d36a6b89461
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-13 18:38:01 -0500

    Agregar tabs en profile de usuario
    - Agregar nuevo template profile para mostrar información de usuario.
    
    - Agregar tabs para mostrar pods, followers y following.
    
    - Agregar parcial para renderizar botón de seguir.

---
+++ app/controllers/users/followers_controller.rb
@@ -0,0 +1,8 @@
+class Users::FollowersController < UsersController
+  before_action :set_user
+  layout "profile"
+
+  def show
+    @followers = @user.followers.with_attached_avatar.page(params[:page])
+  end
+end
--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,4 +1,4 @@
-class Users::RelationshipsController < ApplicationController
+class Users::RelationshipsController < UsersController
   before_action :authenticate_user!
   before_action :set_user
 
@@ -19,10 +19,4 @@ class Users::RelationshipsController < ApplicationController
       format.js
     end
   end
-
-  private
-
-    def set_user
-      @user = User.find_by!(username: params[:user_username])
-    end
 end
