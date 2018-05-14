015c9a3a2a94db8ae18625d49895ba873dc9ade1
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-14 00:56:24 -0500

    Agrear counter cache a pods

---
+++ db/migrate/20180514055143_add_pods_count_to_users.rb
@@ -0,0 +1,5 @@
+class AddPodsCountToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :pods_count, :integer, default: 0, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_031050) do
+ActiveRecord::Schema.define(version: 2018_05_14_055143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_031050) do
     t.string "name"
     t.integer "followers_count", default: 0, null: false
     t.integer "following_count", default: 0, null: false
+    t.integer "pods_count", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
