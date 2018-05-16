305e6078a5f7331aaa28f1453ee7dc450af93492
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-16 00:38:26 -0500

    Agregar tests para welcome controller

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
-    assert_response :success
+  class LoggedIn < WelcomeControllerTest
+    test "should render landing on get index" do
+      get root_path
+      assert_response :success
+      assert_match /Welcome to ReddHub/, @response.body
+    end
+  end
+
+  class LoggedOut < WelcomeControllerTest
+    test "should render user home on get index" do
+      sign_in users(:eo)
+      get root_path
+      assert_response :success
+      assert_match /Eo/, @response.body
+      assert_no_match /Welcome to ReddHub/, @response.body
+    end
   end
 end
