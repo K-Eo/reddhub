f7a07ea37e8a88d92afeca1527f8b42ebe20ea5e
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-16 03:50:55 -0500

    Mejorar scripts de gitlab ci
    - Separar scripts para instalar heroku, gems y yarn packages.
    
    - Usar ubuntu como imagen en stages deploy.

---
+++ scripts/setup/gems.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo Setting up gems
+
+gem install bundler --no-ri --no-rdoc
+bundle install --jobs $(nproc) --path vendor/bundle
+
+echo Setting up gems DONE
---
+++ scripts/setup/heroku.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+base_path="tools"
+package_name="heroku.tar.gz"
+
+echo Setting up Heroku.
+
+echo Download Heroku.
+
+if [ -d "$base_path" ]
+then
+  echo Directory tools ready.
+else
+  echo Create tools directory.
+  mkdir $base_path
+fi
+
+if [ -f "$base_path/$package_name" ]
+then
+  echo File $package_name already cached. 
+else
+  echo Downloading heroku.tar.gz.
+  wget -nv -O $base_path/$package_name https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+fi
+
+echo Configure Heroku.
+
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xzf $basepath/$package_name -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+echo Setting up Heroku DONE.
