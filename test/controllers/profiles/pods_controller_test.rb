186fb4d632ba4b5e1f27af0e18120e458df1a078
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-21 20:00:33 -0500

    Agregar test para ver pod

--- test/controllers/profiles/pods_controller_test.rb
+++ test/controllers/profiles/pods_controller_test.rb
@@ -1,7 +1,31 @@
-require 'test_helper'
+require "test_helper"
 
 class Profiles::PodsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "seeing pod" do
+    @user = users(:bilbo)
+    @pod = @user.pods.first
+
+    get user_profile_path(@user.username)
+
+    assert_response :ok
+
+    assert_select "p", text: @pod.content
+    assert_select ".pod", count: 2
+
+    get user_pod_path(@user.username, @pod)
+
+    assert_response :ok
+
+    assert_select ".pod", count: 0
+    assert_select ".card" do
+      assert_select "a[href='/#{@user.username}']" do
+        assert_select "p", text: @user.name
+        assert_select "p", text: "@#{@user.username}"
+      end
+
+      assert_select "p", text: @pod.content
+
+      assert_select "a.action-like"
+    end
+  end
 end
