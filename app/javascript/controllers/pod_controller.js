43ccd4dd08935eb5ac49263e015d609dde500e9a
Diego Lopez Garcia <aud0l@hotmail.com>
2018-05-21 20:05:25 -0500

    Corregir lint y prettier
    - Agregar Turbolinks como global en eslintrc.yml.

--- app/javascript/controllers/pod_controller.js
+++ app/javascript/controllers/pod_controller.js
@@ -3,15 +3,11 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['source']
 
-  initialize() {
-    console.log('Pod')
-  } 
-
   visit(e) {
     const url = this.sourceTarget.getAttribute('data-url')
     const canVisit = $(e.target).closest('.js-action').length === 0
 
-    if (url && canVisit) { 
+    if (url && canVisit) {
       Turbolinks.visit(url)
     }
   }
