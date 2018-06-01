a15858b2637d666d8089912c880b0d1ea2893e67
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-01 17:40:49 -0500

    Refactorizar test de usuario
    - Remover clases en tests.
    
    - Agregar nuevos casos para seguidores.
    
    - Mover traducciones de flash y agragar nuevos.

--- app/controllers/profiles/relationships_controller.rb
+++ app/controllers/profiles/relationships_controller.rb
@@ -2,6 +2,18 @@ class Profiles::RelationshipsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_by_username
 
+  rescue_from ActiveRecord::RecordNotUnique do
+    redirect_to(user_profile_path(@user.username), notice: t(".already", username: @user.username))
+  end
+
+  rescue_from User::NotDifferentUsers do
+    redirect_to(user_profile_path(@user.username), notice: t(".same"))
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to(user_profile_path(current_user.username), notice: t(".missing", username: params[:username]))
+  end
+
   def create
     current_user.follow(@user)
     redirect_to(user_profile_path(@user.username), notice: t(".notice", username: @user.username))
