47797aa8f69a722d16d7efbb878c248f821af44b
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-02 03:47:00 -0500

    Agregar paginas para tos, privacy y disclaimer

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,21 +1,19 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  class LoggedIn < WelcomeControllerTest
-    test "should render landing on get index" do
-      get root_path
-      assert_response :success
-      assert_match /Welcome to ReddHub/, @response.body
-    end
+  test "should render landing on get index" do
+    get root_path
+    assert_response :success
+    assert_match /Welcome to ReddHub/, @response.body
+    assert_select "a[href='#{terms_path}']", text: "Terms"
+    assert_select "a[href='#{privacy_path}']", text: "Privacy"
   end
 
-  class LoggedOut < WelcomeControllerTest
-    test "should render user home on get index" do
-      sign_in users(:bilbo)
-      get root_path
-      assert_response :success
-      assert_match /Bilbo/, @response.body
-      assert_no_match /Welcome to ReddHub/, @response.body
-    end
+  test "should render user home on get index" do
+    sign_in users(:bilbo)
+    get root_path
+    assert_response :success
+    assert_match /Bilbo/, @response.body
+    assert_no_match /Welcome to ReddHub/, @response.body
   end
 end
