417518ecce96fd86ccc9f722dc06c628bda2202c
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-03 01:03:42 -0500

    Refactorizar tests de pods controller

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -4,19 +4,13 @@ class PodsController < ApplicationController
   def create
     @pod = current_user.pods.new(pod_params)
 
-    respond_to do |format|
-      if @pod.save
-        format.html { redirect_to root_path(current_user.username), notice: "Pod was successfully created." }
-        format.js
-      else
-        format.html do
-          @user = current_user
-          @pods = current_user.feed.page(params[:page])
-          @reactions = current_user.reactions_for(@pods, "Pod")
-          render template: "home/show", layout: "home"
-        end
-        format.js
-      end
+    if @pod.save
+      redirect_to root_path(current_user.username), notice: "Pod was successfully created."
+    else
+      @user = current_user
+      @pods = current_user.feed.page(params[:page])
+      @reactions = current_user.reactions_for(@pods, "Pod")
+      render template: "home/show", layout: "home"
     end
   end
 
--- test/controllers/pods_controller_test.rb
+++ test/controllers/pods_controller_test.rb
@@ -5,104 +5,90 @@ class PodsControllerTest < ActionDispatch::IntegrationTest
     @user = users(:bilbo)
   end
 
-  class SignOut < PodsControllerTest
-    def setup
-      super
+  test "creating pod" do
+    sign_in @user
+
+    get root_path
+
+    assert_response :ok
+
+    assert_select "li.pod", count: 2
+    assert_select "form#new_pod" do
+      assert_select "textarea#pod_content"
+      assert_select "input[type=submit][value='Create Pod']"
     end
 
-    test "redirects to login on post create" do
+    assert_difference "@user.pods.count" do
       post pods_path, params: { pod: { content: "Foo bar"  } }
-
-      assert_redirected_to new_user_session_path
     end
 
-    test "unauthorized on post create" do
-      post pods_path, params: { pod: { content: "Foo bar"  } }, xhr: true
+    assert_redirected_to root_path
 
-      assert_response :unauthorized
+    follow_redirect!
+
+    assert_select "li.pod", count: 3
+    assert_select "li.pod" do
+      assert_select "p", text: "Foo bar"
     end
+
+    assert_equal @user, Pod.last.user
+
+    assert_select "div.alert-notice", text: /Pod was successfully created/
   end
 
-  class SignIn < PodsControllerTest
-    def setup
-      super
-      sign_in @user
+  test "creating pod with error" do
+    sign_in @user
+
+    get root_path
+
+    assert_response :ok
+
+    assert_no_difference "@user.pods.count" do
+      post pods_path, params: { pod: { content: ""  } }
     end
 
-    test "creating pod" do
-      get root_path
+    assert_response :success
 
-      assert_response :ok
+    assert_select "li.pod", count: 2
+    assert_select "div", text: /can't be blank/
 
-      assert_select "li.pod", count: 2
-      assert_select "form#new_pod" do
-        assert_select "textarea#pod_content"
-      end
-
-      assert_difference "@user.pods.count" do
-        post pods_path, params: { pod: { content: "Foo bar"  } }
-      end
-
-      assert_redirected_to root_path
-
-      follow_redirect!
-
-      assert_select "li.pod", count: 3
-      assert_select "li.pod" do
-        assert_select "p", text: "Foo bar"
-      end
-
-      assert_select "div.alert-notice", text: /Pod was successfully created/
+    assert_no_difference "@user.pods.count" do
+      post pods_path, params: { pod: { content: ("a" * 281) } }
     end
 
-    test "seeing reactions if pod has errors" do
-      @user.pods.first.reactions.create(user: @user, name: "rage")
-      @user.pods.last.reactions.create(user: @user, name: "+1")
-      get root_path
+    assert_response :success
 
-      assert_response :ok
-      assert_select "li.pod", count: 2
-      assert_select "img[title=':rage:']"
-      assert_select "img[title=':+1:']"
+    assert_select "li.pod", count: 2
+    assert_select "div", text: /is too long/
+  end
 
-      assert_no_difference "@user.pods.count" do
-        post pods_path, params: { pod: { content: "" } }
-      end
+  test "seeing reactions if pod has errors" do
+    sign_in @user
+    @user.pods.first.reactions.create(user: @user, name: "rage")
+    @user.pods.last.reactions.create(user: @user, name: "+1")
 
-      assert_response :ok
-      assert_select "div", text: /can't be blank/
-      assert_select "li.pod", count: 2
-      assert_select "img[title=':rage:']"
-      assert_select "img[title=':+1:']"
+    get root_path
+
+    assert_response :ok
+
+    assert_select "li.pod", count: 2
+    assert_select "img[title=':rage:']"
+    assert_select "img[title=':+1:']"
+
+    assert_no_difference "@user.pods.count" do
+      post pods_path, params: { pod: { content: "" } }
     end
 
-    test "creating pod with error" do
-      get root_path
+    assert_response :ok
+    assert_select "div", text: /can't be blank/
+    assert_select "li.pod", count: 2
+    assert_select "img[title=':rage:']"
+    assert_select "img[title=':+1:']"
+  end
 
-      assert_response :ok
+  test "redirects to login on creating pod" do
+    post pods_path, params: { pod: { content: "Foo bar"  } }
 
-      assert_select "li.pod", count: 2
-      assert_select "form#new_pod" do
-        assert_select "textarea#pod_content"
-      end
-
-      assert_no_difference "@user.pods.count" do
-        post pods_path, params: { pod: { content: ""  } }
-      end
-
-      assert_response :success
-
-      assert_select "li.pod", count: 2
-      assert_select "div", text: /can't be blank/
-
-      assert_no_difference "@user.pods.count" do
-        post pods_path, params: { pod: { content: ("a" * 281) } }
-      end
-
-      assert_response :success
-
-      assert_select "li.pod", count: 2
-      assert_select "div", text: /is too long/
-    end
+    assert_redirected_to new_user_session_path
   end
 end
