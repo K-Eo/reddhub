a7b8e9d29bb4b89249ef867e4c7b1269d9a273f4
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-20 09:56:10 -0500

    Pods::Reactions ahora desciende de ReactionsController

--- app/controllers/pods/reactions_controller.rb
+++ app/controllers/pods/reactions_controller.rb
@@ -1,30 +1,10 @@
-class Pods::ReactionsController < ApplicationController
+class Pods::ReactionsController < ReactionsController
   before_action :authenticate_user!
-  before_action :set_pod
-
-  def destroy
-    @pod.reactions.where(user: current_user).destroy_all
-    redirect_back(fallback_location: root_path)
-  end
-
-  def create
-    @pod.reactions
-      .where(user: current_user, name: sanitize_emoji_name)
-      .first_or_create
-    redirect_back(fallback_location: root_path)
-  end
+  before_action :set_reactable
 
   private
 
-    def sanitize_emoji_name
-      if Reaction::NAMES.include?(params[:name])
-        params[:name]
-      else
-        Reaction::DEFAULT_NAME
-      end
-    end
-
-    def set_pod
-      @pod = Pod.find(params[:pod_id])
+    def set_reactable
+      @reactable = Pod.find(params[:pod_id])
     end
 end
