3a9f576c1e5871af6714981347597043a530da88
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-26 18:41:06 -0500

    Agregar validaciones para title y description en stories

--- lib/reddhub/pod.rb
+++ lib/reddhub/pod.rb
@@ -1,17 +1,18 @@
 module Reddhub
   module Pod
-    STORY_REGEX = /\A# \S.+\n\n\S.+\n\n\S.+/
-    STORY_META_REGEX = /\A# (\S[^\n]+)\n\n(\S[^\n]+)\n\n(.*)\z/m
+    STORY_META_REGEX = /\A# (\S[^\n]+)\n\n(\S[^\n]+)\n\n(\S.*)\z/m
 
     POD = 0
     STORY = 1
 
     class << self
-      def story?(content)
-        content.present? && content.match(STORY_REGEX)
-      end
-
       def parse_story(content)
+        return nil if content.nil?
+
+        matches = content.match(STORY_META_REGEX)
+
+        return nil if matches.nil?
+
         content.match(STORY_META_REGEX).captures
       end
     end
--- test/lib/reddhub/pod_test.rb
+++ test/lib/reddhub/pod_test.rb
@@ -1,53 +1,51 @@
 require "test_helper"
 
 class PodTest < ActiveSupport::TestCase
-  test "check if is story" do
-    content = "# My title\n\nMy description\n\nMy content"
-    assert Reddhub::Pod.story?(content)
-
-    content = "# My title\n\n# My description\n\n#My content"
-    assert Reddhub::Pod.story?(content)
-
-    content = nil
-    assert_not Reddhub::Pod.story?(content)
-
-    content = ""
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "# My title\n\n My description\n\n#My content"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "#  My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "#My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "# My title\n\nMy description\n"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "# My title\n\nMy description\n\n "
-    assert_not Reddhub::Pod.story?(content)
-
-    content = " # My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "## My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-
-    content = "  My title\n\nMy description"
-    assert_not Reddhub::Pod.story?(content)
-  end
-
   test "extracting meta" do
     content = "# My title\n\nMy description\n\nMy content\n\nMy extra content"
     title, description, body = Reddhub::Pod.parse_story(content)
-
     assert_equal "My title", title
     assert_equal "My description", description
     assert_equal "My content\n\nMy extra content", body
+
+    content = "# My title\n\n# My description\n\n# My content"
+    title, description, body = Reddhub::Pod.parse_story(content)
+    assert_equal "My title", title
+    assert_equal "# My description", description
+    assert_equal "# My content", body
+
+    content = nil
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = ""
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "# My title\n\n My description\n\n#My content"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "#  My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "#My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "# My title\n\nMy description\n"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "# My title\n\nMy description\n\n "
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = " # My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "## My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
+    content = "  My title\n\nMy description"
+    assert_nil Reddhub::Pod.parse_story(content)
+
   end
 end
