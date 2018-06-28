aab0e607a5aee4f010a5db6c50edfe1952bcb56c
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-28 13:46:44 -0500

    Agregar common chunks a webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,23 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
+
+environment.plugins.append(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: module => module.context && module.context.indexOf('node_modules') !== -1
+  })
+)
+
+environment.plugins.append(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity
+  })
+)
+
 module.exports = environment
