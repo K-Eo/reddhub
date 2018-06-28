d85c732aeb4a224935aef7dacf3547c748ec7391
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-28 14:33:17 -0500

    Fix rubocop

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -7,7 +7,7 @@ module FontAwesomeHelper
     html_options[:class] = content_class
 
     html = content_tag(:i, nil, html_options)
-    html << ' ' << text.to_s unless text.blank?
+    html << " " << text.to_s unless text.blank?
     html
   end
 end
