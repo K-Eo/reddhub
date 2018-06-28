4a221b68b242ccca49c5073fe6cccdc947865f19
Diego Lopez Garcia <aud0l@hotmail.com>
2018-06-28 13:22:01 -0500

    Mover cropper.css para eliminar warning

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
-import '../behavior'
-
+import 'croppie/croppie.css'
 import '../src/application.scss'
+
+import '../behavior'
 
 const application = Application.start()
 const context = require.context('controllers', true, /.(ts|js)$/)
